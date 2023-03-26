d5b6421357953237b90fcbbd57835b8321a74e85
android course <yousf2050@hotmail.com>
2023-03-27 01:12:01 +0300

    23/3/2023

---
+++ app/src/main/java/com/example/taskthree/MainActivity6.java
@@ -0,0 +1,33 @@
+package com.example.taskthree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.net.URI;
+
+public class MainActivity6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main6);
+//        WebView webView = findViewById(R.id.web_view);
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.getSettings().setLoadWithOverviewMode(true);
+//        webView.getSettings().setUseWideViewPort(true);
+//        webView.getSettings().setDomStorageEnabled(true);
+//        webView.loadUrl("https://www.google.com");
+//
+//        VideoView videoView = findViewById(R.id.video_view);
+//        videoView.setVideoURI(Uri.parse("https://rr4---sn-4g5ednsk.googlevideo.com/videoplayback?expire=1679543445&ei=NXgbZJq0AvqazN0P7ICoqAM&ip=37.19.221.2&id=o-AJOoS9ss4ZTU-aVcXxOikGibMfaJggkRrCcNm3IIYLof&itag=22&source=youtube&requiressl=yes&spc=H3gIhlX4jGJNhvlJ1ML5kYhU9svNz1WdtxvknKkEukdUsVtWTg&vprv=1&mime=video%2Fmp4&ns=oMYh0U-oE66XUh8uRZXY6mgM&cnr=14&ratebypass=yes&dur=251.843&lmt=1673587390736140&fexp=24007246&c=WEB&txp=5311224&n=pShJQ-8a3mmP3Q&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cspc%2Cvprv%2Cmime%2Cns%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgUSN4_1RHSgkm7XpEg6ZmfQnsLeaahwhGNlTjGCdIJ4UCIQDBn_lRAMy93AXMlkUKVYkPFfl0e_cjRmDgBeEsTPgW6g%3D%3D&title=VideoView%20-%20Android%20Studio%20Tutorial&rm=sn-q4fe7y76&req_id=90dae6201e76a3ee&ipbypass=yes&cm2rm=sn-wxuoxu2n-b15l7e,sn-hgnlk7s&redirect_counter=3&cms_redirect=yes&cmsv=e&mh=n1&mip=5.133.27.53&mm=34&mn=sn-4g5ednsk&ms=ltu&mt=1679521481&mv=m&mvi=4&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRgIhALiE7l8es4MnBQwZFil-V2sfdvM3H43H9L2m2e1B2YvHAiEA1YQxtvSTIjo0s0ACq5EbuFPh3LuMFsc_ZM5z0RpIYUM%3D"));
+//        MediaController mediaController = new MediaController(this);
+//        videoView.setMediaController(mediaController);
+//        mediaController.setAnchorView(videoView);
+
+    }
+}
