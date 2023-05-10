570c168248ed76768ce1c3ec186cd6cef9ebe300
youssef ezzeldeen <yousf2050@hotmail.com>
2023-05-10 15:38:16 +0300

    27/3/2023

---
+++ app/src/main/java/com/example/taskthree/CalcGridLayout.java
@@ -0,0 +1,14 @@
+package com.example.taskthree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalcGridLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc_grid_layout);
+    }
+}
---
+++ app/src/main/java/com/example/taskthree/CalcTableLayout.java
@@ -0,0 +1,14 @@
+package com.example.taskthree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalcTableLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc_table_layout);
+    }
+}
